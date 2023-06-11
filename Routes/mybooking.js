6bac851f441d479133edf0554501241cd985b7e7
Utt996 <Ut25122001@gmail.com>
2023-06-11 13:42:31 +0530

    edit api

--- Routes/mybooking.js
+++ Routes/mybooking.js
@@ -2,6 +2,7 @@ const exoress = require('express');
 
 const router = exoress.Router();
 
+
 const MyBooking = require('../Models/MyBooking');
 
 
@@ -25,6 +26,29 @@ router.post('/create', async (req, res) => {
         return res.status(200).json(newBooking);
     } catch (error) {
         return res.status(500).json({message:error.message});
+    }
+});
+
+router.put('/update/:id', async (req, res) => {
+    try {
+        const booking = await MyBooking.findById(req.params.id);
+
+        if(!booking){
+            return res.json({message:'Booking not found'});
+        }
+
+        booking.name = req.body.name || booking.name;
+        booking.to = req.body.to || booking.to;
+        booking.destination = req.body.destination || booking.destination;
+        booking.travelDate = req.body.travelDate || booking.travelDate;
+        booking.amt = req.body.amt || booking.amt;
+
+        await booking.save();
+
+
+        
+    } catch (error) {
+        
     }
 });
 
